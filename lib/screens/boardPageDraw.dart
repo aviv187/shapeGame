b9e1817968bfeb38f966e230f1d338724259dec7
Aviv <aviv@Avivs-MacBook-Pro.local>
2020-11-23 13:01:00 +0200

    keyboard doesnt distrupt screen anymore

--- lib/screens/boardPageDraw.dart
+++ lib/screens/boardPageDraw.dart
@@ -42,52 +42,68 @@ class BoardDraw extends StatelessWidget {
         title: Text('Polygons'),
         elevation: 0,
       ),
-      body: Stack(children: <Widget>[
-        Column(children: <Widget>[
-          SizedBox(
-            height: 35,
+      body: SingleChildScrollView(
+        physics: NeverScrollableScrollPhysics(),
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minWidth: MediaQuery.of(context).size.width,
+            minHeight: MediaQuery.of(context).size.height,
           ),
-          Center(
-              child: Container(
-            width: boardWidth,
-            height: boardWidth * 1.55,
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              borderRadius: BorderRadius.all(Radius.circular(10.0)),
-              border: Border.all(
-                width: 3,
-                color: Colors.grey[700],
-              ),
-            ),
-            child: Stack(
-              children: [
-                // draw the board
-                Stack(
-                  children: tileList.map((Tile tile) {
-                    return BoardTile(
-                      sides: 4,
-                      size: tileSize,
-                      leftPosition: makeTileIntToSizeForPaint(
-                          tile.leftPosition, tileSize),
-                      topPosition:
-                          makeTileIntToSizeForPaint(tile.topPosition, tileSize),
-                      color: Colors.grey,
-                    );
-                  }).toList(),
-                ),
+          child: IntrinsicHeight(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Stack(children: <Widget>[
+                  Column(children: <Widget>[
+                    SizedBox(
+                      height: 35,
+                    ),
+                    Center(
+                        child: Container(
+                      width: boardWidth,
+                      height: boardWidth * 1.55,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        border: Border.all(
+                          width: 3,
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                      child: Stack(
+                        children: [
+                          // draw the board
+                          Stack(
+                            children: tileList.map((Tile tile) {
+                              return BoardTile(
+                                sides: 4,
+                                size: tileSize,
+                                leftPosition: makeTileIntToSizeForPaint(
+                                    tile.leftPosition, tileSize),
+                                topPosition: makeTileIntToSizeForPaint(
+                                    tile.topPosition, tileSize),
+                                color: Colors.grey,
+                              );
+                            }).toList(),
+                          ),
+                        ],
+                      ),
+                    )),
+                  ]),
+                  // all the statful widgets in the board
+                  BoardWidgets(
+                    tileList: tileList,
+                    colNum: colNum,
+                    rowNum: rowNum,
+                    tileSize: tileSize,
+                    boardWidth: boardWidth,
+                  ),
+                ]),
               ],
             ),
-          )),
-        ]),
-        // all the statful widgets in the board
-        BoardWidgets(
-          tileList: tileList,
-          colNum: colNum,
-          rowNum: rowNum,
-          tileSize: tileSize,
-          boardWidth: boardWidth,
+          ),
         ),
-      ]),
+      ),
     );
   }
 }
